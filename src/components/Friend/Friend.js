e7564296b2b8a37beddf8d9249d7df0764b289fd
Mahede-Hasan <mahedehasan.cd91@gmail.com>
2022-09-04 00:00:02 -0700

    updated

--- src/components/Friend/Friend.js
+++ src/components/Friend/Friend.js
@@ -6,13 +6,14 @@ const Friend = (props) => {
     const { name, email, id } = props.friend;
     const navigate = useNavigate()
     const handleClick = () => {
-        navigate('/friend/'+ id)
+        navigate('/friend/' + id)
     }
     return (
         <div className='px-2 py-6 bg-blue-300 mx-2 my-4'>
             <h2 className='text-3xl'>Name: {name}</h2>
             <h4 className='text-1xl'>Email: {email}</h4>
-            <Link to={"/friend/"+id}>show</Link>
+            <p>{id}</p>
+            <Link to={"/friend/" + id}>show</Link>
             <button onClick={handleClick} className='bg-pink-600 text-white px-6 py-2 mt-6  text-lg rounded font-bold'>click to details {id}</button>
         </div>
     );
--- src/components/FriendDetail/FriendDetail.js
+++ src/components/FriendDetail/FriendDetail.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
 const FriendDetail = () => {
-    let { friendId } = useParams;
+    const { friendId } = useParams;
     const [friend, setFriend] = useState({})
     useEffect(() => {
         const url = `https://jsonplaceholder.typicode.com/users/${friendId}`
@@ -12,7 +12,6 @@ const FriendDetail = () => {
     }, [])
     return (
         <div>
-           
             <h2>Friends Details {friendId}</h2>
             <h3>Name: {friend.name}</h3>
         </div>
